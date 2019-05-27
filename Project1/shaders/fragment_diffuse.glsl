1dffcc19a68d9d21e57c1bc3c311e758330cce2d
florian <flo.deckert@gmail.com>
2019-05-27 14:29:13 +0200

    add percentage-closer filtering PCF to the shadhows

--- Project1/shaders/fragment_diffuse.glsl
+++ Project1/shaders/fragment_diffuse.glsl
@@ -32,12 +32,19 @@ uniform Material material;
 		float currentDepth = projCoords.z;
 		// check whether current frag pos is in shadow
 		float bias = 0.005;
-		float shadow; 
+		float shadow = 0.0; 
 
-		if(projCoords.z > 1.0)
-			shadow = 0.0;
-		else
-			shadow = currentDepth - bias > closestDepth  ? 1.0 : 0.0; 
+		if(projCoords.z <= 1.0) {
+			// PCF
+			vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
+			for(int x = -1; x <= 1; ++x) {
+				for(int y = -1; y <= 1; ++y) {
+					float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r; 
+					shadow += currentDepth - bias > pcfDepth  ? 1.0 : 0.0;        
+				}    
+			}
+			shadow /= 9.0;		
+		}
 
 		return shadow;
 	}  
