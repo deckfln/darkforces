adbd9fa51277440c4b1ffc1bb80a3f17a90ce574
florian <flo.deckert@gmail.com>
2019-11-24 19:37:20 +0100

    FIX bug #4: marie-jane "running" gets suspened at the end of the animation loop. the last frame of the loop is already a clone of the first one

--- Project1/framework/fwAnimation.cpp
+++ Project1/framework/fwAnimation.cpp
@@ -14,35 +14,26 @@ fwAnimation::fwAnimation(std::string name, time_t duration, fwBoneInfoAnimation*
 
 void fwAnimation::reset(void)
 {
-	m_currentTime = 0;
+	m_currentTime = m_keyframes[0];
 }
 
 void fwAnimation::keyframes(std::map<time_t, bool>& keyframes)
 {
 	
 	m_nbKeyframes = keyframes.size();
-	m_keyframes = new time_t[m_nbKeyframes + 1]();
+	m_keyframes = new time_t[m_nbKeyframes]();
 
 	int i = 0;
-	time_t first = -1, last = 0;
 
 	for (auto const& keyframe : keyframes) {
-		if (first < 0) {
-			first = keyframe.first;
-		}
-
-		last = keyframe.first;
-		m_keyframes[i] = last;
-
+		m_keyframes[i] = keyframe.first;
 		i++;
 	}
-
-	m_keyframes[m_nbKeyframes] = first + last;
 }
 
 void fwAnimation::update(time_t elapsed, glm::mat4 *target, glm::mat4& GlobalInverseTransform)
 {
-	m_currentTime += elapsed/32;
+	m_currentTime += elapsed;
 
 	// find the interval
 	glm::mat4 identity(1);
@@ -53,9 +44,9 @@ void fwAnimation::update(time_t elapsed, glm::mat4 *target, glm::mat4& GlobalInv
 	while (m_currentTime >= next_t) {
 		m_currentFrame++;
 
-		if (m_currentFrame >= m_nbKeyframes) {
+		if (m_currentFrame >= m_nbKeyframes - 1) {
 			m_currentFrame = 0;
-			m_currentTime = 0;
+			m_currentTime = m_keyframes[0];
 
 			prev_t = m_keyframes[0];
 			next_t = m_keyframes[1];
@@ -64,6 +55,8 @@ void fwAnimation::update(time_t elapsed, glm::mat4 *target, glm::mat4& GlobalInv
 			prev_t = m_keyframes[m_currentFrame];
 			next_t = m_keyframes[m_currentFrame + 1];
 		}
+
+		std::cout << m_currentFrame << ":" << m_currentTime << " ! " << std::endl;
 	}
 
 	time_t delta = (m_currentTime - prev_t);
@@ -73,10 +66,6 @@ void fwAnimation::update(time_t elapsed, glm::mat4 *target, glm::mat4& GlobalInv
 	debug += x;
 	*/
 	// interpolate the fwAnimationKeyframes
-	if (m_currentFrame + 1 >= m_nbKeyframes) {
-		// wrap the cycle
-		next_t = m_keyframes[0];
-	}
 	m_skeleton->interpolate(prev_t, next_t, inbetween_t, target, identity, GlobalInverseTransform);
 }
 
